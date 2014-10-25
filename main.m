b93eb1120094c48ec2e20976990e9c3c6b301be4
pitau07 <julian.decreuse@utbm.fr>
2014-10-25 15:01:01 +0200

    profils droit et gauche

--- main.m
+++ main.m
@@ -1,10 +1,8 @@
-clear
+clc
+close all
 
 %-- Image Load --%
 I=imread('app.tif');
-
-%- Inversion de l'image -%
-Ib= 255-I;
 
 %-- Recherche des lignes --%
 Lignes = seekLines(I);
@@ -17,6 +15,7 @@ Rectangles=seekRectangles(I,Lignes,Colonnes);
 drawRectangles(I,Rectangles);
 save('Rect.mat','Rectangles','-ascii');
 
-
-
- 
+%-- Dist Euclidienne minimum --%
+for i=0:10:90 
+    Vecteur(i+1:i+10,1:20) = seek_LandR_Profiles( I,5, Rectangles(2*i+1:2*i+20,1:4) );
+end
